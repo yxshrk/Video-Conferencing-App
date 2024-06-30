32b9a1930627f2df80c36cd0ef81f6ed562fdb16
yxshrk <yrk17122002@gmail.com>
2024-06-30 13:04:51 +0800

    done again

--- my-app/src/app/(root)/(home)/page.tsx
+++ my-app/src/app/(root)/(home)/page.tsx
@@ -1,21 +1,41 @@
+'use client';
+
 import MeetingTypeList from '@/components/MeetingTypeList';
 import React from 'react'
+import { useGetCalls } from '../../../../hooks/useGetCalls';
+import { Call } from '@stream-io/video-react-sdk';
 
 const Home = () => {
   const now = new Date();
-
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  
+  const time = now.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'Asia/Singapore'
+  });
+  
   const date = (new Intl.DateTimeFormat('en-US', {
-    dateStyle: 'full'
+    dateStyle: 'full',
+    timeZone: 'Asia/Singapore'
   })).format(now);
+
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+    useGetCalls();
 
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
         <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-          <h2 className='glassmorphism max-w-[270px] rounded py-3 text-center text-base font-normal'>
-            Upcoming Meeting at: 12:30 PM
+        {upcomingCalls && upcomingCalls.length > 0 && upcomingCalls[0] ? (
+          <h2 className='glassmorphism flex max-w-[400px] rounded py-3 px-2 text-center justify-center text-base font-normal'>
+            Upcoming Meeting: {upcomingCalls[0]?.state?.startsAt.toLocaleString()}
           </h2>
+        ) : (
+          <h2 className='glassmorphism max-w-[270px] rounded py-3 text-center text-base font-normal'>
+            No Upcoming Meetings
+          </h2>
+
+        )}
           <div className='flex flex-col gap-2'>
             <h1 className='text-4xl font-extrabold lg:text-7xl'>
               {time}
